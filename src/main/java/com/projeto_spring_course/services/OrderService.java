3055c338b520cd875a2c569bd17f5cc6e508332e
Enzo Camp <enzo.flap@hotmail.com>
2025-06-03 20:34:28 -0300

    Including JsonIgnore in associations and JsonFormat to Instant

---
+++ src/main/java/com/projeto_spring_course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.projeto_spring_course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto_spring_course.entities.Order;
+import com.projeto_spring_course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
