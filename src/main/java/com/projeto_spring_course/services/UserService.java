56ceb39466a882f0f4d4f0f8e7aabf50b5e78b76
Enzo Camp <enzo.flap@hotmail.com>
2025-06-02 23:41:51 -0300

    Creating service layer and component registration

---
+++ src/main/java/com/projeto_spring_course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.projeto_spring_course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto_spring_course.entities.User;
+import com.projeto_spring_course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
