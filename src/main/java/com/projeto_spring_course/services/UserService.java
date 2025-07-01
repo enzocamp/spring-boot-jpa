d6f234234edbede7d73de0933fbef827d08da574
Enzo Camp <enzo.flap@hotmail.com>
2025-07-01 19:23:56 -0300

    Personalizing exceptions, findById UserSerice

--- src/main/java/com/projeto_spring_course/services/UserService.java
+++ src/main/java/com/projeto_spring_course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projeto_spring_course.entities.User;
 import com.projeto_spring_course.repositories.UserRepository;
+import com.projeto_spring_course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
